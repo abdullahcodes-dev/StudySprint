f1972adfac21610da9aa656fb214f77121ace3e3
Muhammad Abdullah <mabdullah20089@gmail.com>
2026-07-10 16:50:00 +0500

    feat(tasks): implement dynamic task creation

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log("StudySprint JS Connected!");
+const taskInput = document.querySelector("input");
+
+const addTaskButton = document.querySelector("button");
+
+const taskList = document.querySelector("ul");
+
+
+addTaskButton.addEventListener("click", function(event) {
+    event.preventDefault();
+
+    const task = taskInput.value;
+
+    if (task.trim() === "") {
+        return;
+    }
+
+    const newTask = document.createElement("li");
+
+    newTask.textContent = task;
+
+    taskList.appendChild(newTask);
+
+    taskInput.value = "";
+
+})
